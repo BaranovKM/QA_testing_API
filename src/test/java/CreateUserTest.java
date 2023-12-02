ce7a0b5d7ed0898748e9889b590be83c53fbabb1
Konstantine Baranov <irongrol@gmail.com>
2023-12-02 16:45:00 +0300

    added additional allure annotations

--- src/test/java/CreateUserTest.java
+++ src/test/java/CreateUserTest.java
@@ -1,12 +1,12 @@
 import io.qameta.allure.*;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.apache.http.protocol.HTTP;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.SpecificationQuerier;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.net.HttpURLConnection;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
@@ -16,48 +16,73 @@ import static java.net.HttpURLConnection.HTTP_CREATED;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CreateUserTest extends BaseTest {
+    @Test
     @DisplayName("POST for single user")
     @Description("Make POST request for single user and validate json in response")
-    @Link("http://jira.com/test-12345")
-    @Feature("API tests")
-    @Owner("Baranov K.M.")
     @Severity(SeverityLevel.CRITICAL)
-    @Test
+    //todo move values to separeted class
+    @Owner("Baranov K.M.")
+    @Epic("Main system's api")
+    @Feature("User creation")
+    @Story("Endpoint for creating a single user")
+    @Link("http://jira.com/ticket-12345")
+    @TmsLink("http://testrail.com/test-12345")
     void createUserTest() {
         String personName = "Horus Lupercal";
         String personJob = "Warmaster";
         String id = "id";
         String createdAt = "createdAt";
-        LocalDateTime userCreatedAt;
 
         Person person = new Person(personName, personJob);
 
-        Response response =
-                given()
-                        .spec(getDefaultRequestSpecification())
-                        .body(person)
-                    .when()
-                        .post(USERS_PATH)
-                    .then()
-                        .statusCode(HTTP_CREATED)
-                        .contentType(ContentType.JSON)
-                        .time(Matchers.lessThan(5000L))
-                        .extract()
-                        .response();
+        Allure.step("Make POST request");
 
-        assertThat(response.as(Map.class).keySet())
-                .isNotNull()
-                .hasSize(2)
-                .containsOnly(id, createdAt);
+        RequestSpecification request = given()
+                .spec(getDefaultRequestSpecification())
+                .body(person);
 
-        assertThat(response.jsonPath().getInt(id))
-                .isNotNull()
-                .isNotNegative()
-                .isGreaterThan(0);
+        Response response = request
+                .when()
+                .post(USERS_PATH)
+                .then()
+                .statusCode(HTTP_CREATED)
+                .contentType(ContentType.JSON)
+                .time(Matchers.lessThan(5000L))
+                .extract()
+                .response();
 
-        userCreatedAt = LocalDateTime.parse(response.jsonPath().getString(createdAt), DateTimeFormatter.ISO_ZONED_DATE_TIME);
+        Allure.addAttachment(
+                "request",
+                "application/json",
+                SpecificationQuerier.query(request).getBody().toString(),
+                ".json");
 
-        assertThat(userCreatedAt).isAfter(LocalDateTime.now().minusHours(4));//server time has huge delay
+        Allure.addAttachment(
+                "response",
+                "application/json",
+                response.asPrettyString(),
+                ".json");
+
+        Allure.step("Validate response", stepContext -> {
+            Allure.step("Check response contain only user id and timestamp");
+            assertThat(response.as(Map.class).keySet())
+                    .isNotNull()
+                    .hasSize(2)
+                    .containsOnly(id, createdAt);
+
+            Allure.step("Check user id");
+            assertThat(response.jsonPath().getInt(id))
+                    .isNotNull()
+                    .isNotNegative()
+                    .isGreaterThan(0);
+
+            LocalDateTime userCreatedAt = LocalDateTime.parse(
+                    response.jsonPath().getString(createdAt),
+                    DateTimeFormatter.ISO_ZONED_DATE_TIME);
+
+            Allure.step("Check timestamp");
+            assertThat(userCreatedAt).isAfter(LocalDateTime.now().minusHours(4));//server time has huge delay
+        });
 
     }
 }
