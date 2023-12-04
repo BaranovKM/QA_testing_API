14f9f1ad31195ff4c78ccbc7a194534a6ecf7913
Konstantine Baranov <irongrol@gmail.com>
2023-12-04 16:27:03 +0300

    refactoring test for remove user

--- src/test/java/DeleteUserTest.java
+++ src/test/java/DeleteUserTest.java
@@ -1,12 +1,18 @@
 import io.qameta.allure.*;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
 import static allure.AllureMarks.*;
 import static allure.AllureMarks.TEST_123;
 import static io.qameta.allure.SeverityLevel.CRITICAL;
+import static io.qameta.allure.util.ResultsUtils.PARENT_SUITE_LABEL_NAME;
 import static io.restassured.RestAssured.given;
+import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
 import static java.net.HttpURLConnection.HTTP_NO_CONTENT;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,21 +28,32 @@ public class DeleteUserTest extends BaseTest {
     @Story(REMOVE_USER)
     @Link(TICKET_123)
     @TmsLink(TEST_123)
+    @Tags({@Tag("api"), @Tag("smoke"), @Tag("regression")})
     void deleteUserTest() {
         String deletedPersonId = "10";
 
+        Allure.label(PARENT_SUITE_LABEL_NAME, REMOVE_USER);
         Allure.step("Make DELETE request");
-        Response response = given()
-                .spec(getDefaultRequestSpecification())
+        Response response =
+                given()
+                        .spec(getDefaultRequestSpecification())
                 .when()
-                .delete(USERS_PATH_WITH_PARAMS, deletedPersonId)
-                .then()
-                .statusCode(HTTP_NO_CONTENT)
-                .extract()
-                .response();
+                        .delete(USERS_PATH_WITH_PARAMS, deletedPersonId);
+
+        Allure.step("Check status code 204");
+        response.then().statusCode(HTTP_NO_CONTENT);
 
         Allure.step("Check response body is empty");
         assertThat(response.asString()).isEmpty();
 
+        Allure.step("Try to get removed user and check status code 404");
+        given()
+                .spec(getDefaultRequestSpecification())
+        .when()
+                .get(USERS_PATH_WITH_PARAMS, deletedPersonId)
+        .then()
+                .contentType(ContentType.JSON)
+                .statusCode(HTTP_NOT_FOUND)
+                .body(Matchers.is(EMPTY_JSON));
     }
 }
